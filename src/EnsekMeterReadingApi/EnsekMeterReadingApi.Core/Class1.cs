56b503dac943915b576c29fcad1c3599d5496f95
Márcio Brandão <evandro_m_brandao@hotmail.com>
2025-07-24 16:31:03 +0100

    Created Solution and Projects; Added Task Data

---
+++ src/EnsekMeterReadingApi/EnsekMeterReadingApi.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EnsekMeterReadingApi.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/EnsekMeterReadingApi/EnsekMeterReadingApi.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EnsekMeterReadingApi.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
